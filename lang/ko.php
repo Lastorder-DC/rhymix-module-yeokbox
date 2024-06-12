a798fe4f5d4645729a3eeea374753d1424706797
Lastorder <18280396+Lastorder-DC@users.noreply.github.com>
2024-06-12 16:57:05 +0900

    다국어화

--- lang/ko.php
+++ lang/ko.php
@@ -1,4 +1,7 @@
 <?php
 
 $lang->cmd_yeokbox = '역박스 커스텀';
+$lang->cmd_yeokbox_board_addon_config = '게시판 추가기능';
+$lang->cmd_yeokka_member_srl = '추천을 표시할 회원 번호';
+$lang->cmd_about_yeokka_member_srl = '게시판에 추천여부를 표시할 회원의 번호를 입력하세요.';
 $lang->msg_yeokbox_invalid_member_srl = '올바르지 않은 회원번호';
