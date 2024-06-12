33c3bee8b34e71966e6418b27ce0364902c29a5a
Lastorder <18280396+Lastorder-DC@users.noreply.github.com>
2024-06-12 15:13:50 +0900

    afterGetDocumentList 트리거 구현 시작

--- controllers/EventHandlers.php
+++ controllers/EventHandlers.php
@@ -12,36 +12,12 @@ namespace Rhymix\Modules\Yeokbox\Controllers;
 class EventHandlers extends Base
 {
 	/**
-	 * 트리거 예제: 새 글 작성시 실행
-	 * 
-	 * 주의: 첨부파일이 있는 경우 아직 작성하지 않았어도 이 함수가 실행될 수 있음
+	 * 여까 추천 글 체크
 	 * 
 	 * @param object $obj
 	 */
-	public function afterInsertDocument($obj)
+	public function afterGetDocumentList($obj)
 	{
-	
-	}
-	
-	/**
-	 * 트리거 예제: 글 수정시 실행
-	 * 
-	 * 주의: 첨부파일이 있는 경우 최종 작성 시점에 이 함수가 실행될 수 있음
-	 * 
-	 * @param object $obj
-	 */
-	public function afterUpdateDocument($obj)
-	{
-	
-	}
-	
-	/**
-	 * 트리거 예제: 글 삭제시 실행
-	 * 
-	 * @param object $obj
-	 */
-	public function afterDeleteDocument($obj)
-	{
-	
+		debugPrint($obj);
 	}
 }
