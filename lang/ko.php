aea02433d5a1419569435c6b3b688afcb0763f4b
Lastorder <18280396+Lastorder-DC@users.noreply.github.com>
2024-06-23 21:05:11 +0900

    념글컷 설정 추가

--- lang/ko.php
+++ lang/ko.php
@@ -5,3 +5,5 @@ $lang->cmd_yeokbox_board_addon_config = '게시판 추가기능';
 $lang->cmd_yeokka_member_srl = '추천을 표시할 회원 번호';
 $lang->cmd_about_yeokka_member_srl = '게시판에 추천여부를 표시할 회원의 번호를 입력하세요.';
 $lang->msg_yeokbox_invalid_member_srl = '올바르지 않은 회원번호';
+$lang->cmd_vote_count = '념글컷';
+$lang->msg_yeokbox_invalid_member_srl = '념글에 해당하는 추천수 컷';
--- models/Config.php
+++ models/Config.php
@@ -36,6 +36,24 @@ class Config
 		self::$_cache->yeokka_member_srl = self::$_cache->yeokka_member_srl ?: 4;
 		return self::$_cache;
 	}
+
+	/**
+	 * 모듈 설정을 가져오는 함수.
+	 * 
+	 * 캐시 처리되기 때문에 ModuleModel을 직접 호출하는 것보다 효율적이다.
+	 * 모듈 내에서 설정을 불러올 때는 가급적 이 함수를 사용하도록 한다. 
+	 * 
+	 * @return object
+	 */
+	public static function getVoteCount()
+	{
+		if (self::$_cache === null)
+		{
+			self::$_cache = ModuleModel::getModuleConfig('yeokbox') ?: new \stdClass;
+		}
+		self::$_cache->vote_count = self::$_cache->vote_count ?: 10;
+		return self::$_cache->vote_count;
+	}
 	
 	/**
 	 * 모듈 설정을 저장하는 함수.
