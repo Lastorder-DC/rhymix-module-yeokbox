0e062735da0480e8e747c7b6f36e1141375b9432
Lastorder <18280396+Lastorder-DC@users.noreply.github.com>
2024-07-09 09:43:40 +0900

    WIP

---
+++ controllers/CommentFunc.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Rhymix\Modules\Yeokbox\Controllers;
+
+use Rhymix\Modules\Yeokbox\Models\Config as ConfigModel;
+use BaseObject;
+use Context;
+
+/**
+ * 역박스 커스텀
+ * 
+ * Copyright (c) Lastorder-DC
+ * 
+ * Generated with https://www.poesis.org/tools/modulegen/
+ */
+class CommentFunc extends Base
+{
+	/**
+	 * 댓글 랜덤 추첨
+	 */
+	public function procYeokboxPickComment()
+	{
+		// 현재 설정 상태 불러오기
+		$config = ConfigModel::getConfig();
+	}
+}
