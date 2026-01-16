fc12e825576d0757472db9dc242ff595efe8461f
Lastorder-DC <18280396+Lastorder-DC@users.noreply.github.com>
2026-01-16 11:41:14 +0900

    optimize eventhandlers

--- controllers/EventHandlers.php
+++ controllers/EventHandlers.php
@@ -13,17 +13,14 @@ use DiceCalc\Calc as Calc;
 
 /**
  * 역박스 커스텀
- * 
- * Copyright (c) Lastorder-DC
- * 
- * Generated with https://www.poesis.org/tools/modulegen/
+ * * Copyright (c) Lastorder-DC
+ * * Generated with https://www.poesis.org/tools/modulegen/
  */
 class EventHandlers extends Base
 {
 	/**
 	 * 첨부파일 뱃지정보 생성용 함수
-	 * 
-	 * @param DocumentItem $doc
+	 * * @param DocumentItem $doc
 	 */
 	private function getMediaBadgeData($doc) {
 		$attach_list = $doc->getUploadedFiles();
@@ -34,17 +31,22 @@ class EventHandlers extends Base
 		$youtube_attach = false;
 		$chzzk_attach = false;
 
+		// 첨부파일 검사: 이미지와 비디오가 모두 발견되면 루프 중단 (최적화)
 		if ($attach_list && count($attach_list) > 0) {
 			foreach ($attach_list as $val) {
-				if (strpos($val->mime_type, 'image') !== false) {
+				if (!$image_attach && strpos($val->mime_type, 'image') !== false) {
 					$image_attach = true;
 				}
-				if (strpos($val->mime_type, 'video') !== false) {
+				if (!$video_attach && strpos($val->mime_type, 'video') !== false) {
 					$video_attach = true;
+				}
+				if ($image_attach && $video_attach) {
+					break;
 				}
 			}
 		}
 
+		// iframe 태그 검사
 		if (preg_match('/<iframe[^>]*src=["\'](?:https?:)?\/\/(?:[\w-]+\.)*(?:youtube\.com|youtu\.be)/i', $content)) {
 			$youtube_attach = true;
 		}
@@ -64,64 +66,71 @@ class EventHandlers extends Base
 
 	/**
 	 * 여까 추천 글 체크 및 첨부파일 뱃지정보 생성
-	 * 
-	 * @param object $obj
+	 * * @param object $obj
 	 */
 	public function afterGetDocumentList($obj) {
+		if (empty($obj->data)) {
+			return;
+		}
+
 		$config = ConfigModel::getConfig();
 		$cacheKeyVote = 'yeokbox_vote_' . $config->yeokka_member_srl;
 		$cacheKeyAttachBadge = 'attach_badge_v3'; 
 
-		$voteData = Cache::get($cacheKeyVote);
-		$badgeData = Cache::get($cacheKeyAttachBadge);
+		// 캐시 가져오기
+		$voteData = Cache::get($cacheKeyVote) ?? [];
+		$badgeData = Cache::get($cacheKeyAttachBadge) ?? [];
 
-		if($voteData === null) {
-			$voteData = [];
-		}
-		if($badgeData === null) {
-			$badgeData = [];
-		}
+		$voteUpdated = false;
+		$badgeUpdated = false;
 
 		foreach ($obj->data as &$doc) {
 			$docSrl = $doc->get('document_srl');
 
-			if(!array_key_exists($docSrl, $voteData)) {
+			if (!isset($voteData[$docSrl])) {
 				$args = new \stdClass();
 				$args->member_srl = $config->yeokka_member_srl;
 				$args->document_srl = $docSrl;
 				$output = executeQuery('document.getDocumentVotedLogInfo', $args);
+				
 				$voteData[$docSrl] = ($output->data->count >= 1 ? "Y" : "N");
+				$voteUpdated = true;
 			}
 
-			if(!array_key_exists($docSrl, $badgeData)) {
+			if (!isset($badgeData[$docSrl])) {
 				$badgeData[$docSrl] = $this->getMediaBadgeData($doc);
+				$badgeUpdated = true;
 			}
 		}
-		Cache::set($cacheKeyVote, $voteData);
-		Cache::set($cacheKeyAttachBadge, $badgeData);
+
+		// 변경사항이 있을 때만 캐시 저장
+		if ($voteUpdated) {
+			Cache::set($cacheKeyVote, $voteData);
+		}
+		if ($badgeUpdated) {
+			Cache::set($cacheKeyAttachBadge, $badgeData);
+		}
 	}
 
 	/**
 	 * 글 업데이트시 첨부파일 뱃지 캐시 삭제
-	 * 
-	 * @param object $obj
+	 * * @param object $obj
 	 */
 	public function afterUpdateDocument($obj) {
 		$document_srl = $obj->document_srl;
-
 		$cacheKeyAttachBadge = 'attach_badge_v3'; 
+		
 		$badgeData = Cache::get($cacheKeyAttachBadge);
-		if($badgeData === null) {
-			$badgeData = [];
+		
+		if ($badgeData !== null && isset($badgeData[$document_srl])) {
+			unset($badgeData[$document_srl]);
+			Cache::set($cacheKeyAttachBadge, $badgeData);
 		}
-
-		unset($badgeData[$document_srl]);
-		Cache::set($cacheKeyAttachBadge, $badgeData);
 	}
 
 	public function afterUpdateVotedCount($obj) {
 		$config = ConfigModel::getConfig();
-		if($config->yeokka_member_srl != \MemberModel::getLoggedMemberSrl()) {
+		if ($config->yeokka_member_srl != \MemberModel::getLoggedMemberSrl()) {
 			return;
 		}
 
@@ -130,7 +139,7 @@ class EventHandlers extends Base
 
 	public function afterUpdateVotedCountCancel($obj) {
 		$config = ConfigModel::getConfig();
-		if($config->yeokka_member_srl != \MemberModel::getLoggedMemberSrl()) {
+		if ($config->yeokka_member_srl != \MemberModel::getLoggedMemberSrl()) {
 			return;
 		}
 
@@ -140,50 +149,59 @@ class EventHandlers extends Base
 	public function updateVoteCache($obj, $vote) {
 		$config = ConfigModel::getConfig();
 		$cacheKey = 'yeokbox_vote_' . $config->yeokka_member_srl;
-
 		$docSrl = $obj->document_srl;
-		$voteData = Cache::get($cacheKey);
-		if($voteData === null) {
-			$voteData = [];
-		}
 
-		$voteData[$docSrl] = $vote;
-		Cache::set($cacheKey, $voteData);
+		$voteData = Cache::get($cacheKey) ?? [];
+
+		// 값이 다를 경우에만 캐시 업데이트 수행
+		if (!isset($voteData[$docSrl]) || $voteData[$docSrl] !== $vote) {
+			$voteData[$docSrl] = $vote;
+			Cache::set($cacheKey, $voteData);
+		}
 	}
 
 	public static function sendNewDocNotification($obj) {
 		// 현재 설정 상태 불러오기
 		$config = ConfigModel::getConfig();
-		$nick = "여까";
+		$yeokkaSrl = intval($config->yeokka_member_srl);
+		$objMemberSrl = intval($obj->member_srl);
 
 		// 작성자가 여까/핫산이 아니면 리턴 
-		if($obj->member_srl == intval($config->yeokka_member_srl)) {
+		if ($objMemberSrl === $yeokkaSrl) {
 			$nick = "여까";
-		} elseif($obj->member_srl == 4) {
+		} elseif ($objMemberSrl === 4) {
 			$nick = "핫산";
 		} else {
 			return;
 		}
 
 		$args = new \stdClass();
-		$args->member_srl = $obj->member_srl;
+		$args->member_srl = $objMemberSrl;
 		$output = executeQueryArray('yeokbox.getFriendList', $args, ['member.member_srl', 'nick_name']);
 
+		if (!count($output->data)) {
+			return;
+		}
+
 		$oNcenterliteController = getController('ncenterlite');
+		$baseMessage = $nick . " 새 글 알림! - " . $obj->title;
+		$targetUrl = "/" . $obj->document_srl;
+
 		foreach($output->data as $friend) {
 			$message = new \stdClass();
-			$message->summary = $nick . " 새 글 알림! - " . $obj->title;
-			$message->subject = $message->summary;
-			$msg = $oNcenterliteController->sendNotification(4, $friend->member_srl, $message, "/" . $obj->document_srl, $obj->document_srl);
+			$message->summary = $baseMessage;
+			$message->subject = $baseMessage;
+			$oNcenterliteController->sendNotification(4, $friend->member_srl, $message, $targetUrl, $obj->document_srl);
 		}
 	}
 
 	public function afterPublishDocument($obj) {
 		// 현재 설정 상태 불러오기
 		$config = ConfigModel::getConfig();
+		$memberSrl = intval($obj->member_srl);
 
 		// 작성자가 여까/핫산이 아니면 리턴 
-		if($obj->member_srl != intval($config->yeokka_member_srl) && $obj->member_srl != 4) {
+		if ($memberSrl !== intval($config->yeokka_member_srl) && $memberSrl !== 4) {
 			return;
 		}
 
@@ -193,39 +211,35 @@ class EventHandlers extends Base
 	}
 
 	public function beforeInsertContent(&$obj) {
-		$content = $obj->content;
-
 		// 댓글 내용에서 {문자} 형태의 문자열을 찾아서 다이스 롤링
 		$pattern = '/\{([^{}]+)\}/';
-		preg_match_all($pattern, $content, $matches, PREG_SET_ORDER);
-		foreach ($matches as $match) {
+		
+		$obj->content = preg_replace_callback($pattern, function ($match) {
 			$originalRollString = $match[0];
 			$expression = $match[1];
-
+			
 			try {
 				$calc = new Calc($expression);
 				$diceText = $calc->infix();
 				$rollResult = $calc();
 			} catch (\Exception $e) {
 				// 계산식이 잘못된 경우 원래 문자열 유지
-				continue;
+				return $originalRollString;
 			}
 
 			// diceText나 rollResult가 비어있으면 원래 문자열 유지
-			if(!$diceText || !$rollResult) {
-				continue;
+			if (!$diceText || !$rollResult) {
+				return $originalRollString;
 			}
 
 			// diceText와 rollResult가 동일하면 rollResult만 표시
-			if($diceText == $rollResult) {
-				$content = preg_replace('/' . preg_quote($originalRollString, '/') . '/', $rollResult, $content, 1);
-				continue;
+			if ($diceText == $rollResult) {
+				return $rollResult;
 			}
 
-			// 여러 다이스 롤을 올바르게 처리하기 위해 첫번째 다이스만 변환
-			$content = preg_replace('/' . preg_quote($originalRollString, '/') . '/', $diceText . ' = ' . $rollResult, $content, 1);
-		}
+			// 계산식과 결과를 함께 표시
+			return $diceText . ' = ' . $rollResult;
 
-		$obj->content = $content;
+		}, $obj->content);
 	}
 }
